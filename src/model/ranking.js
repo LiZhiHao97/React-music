cfa6099ff05f47ffa6b3df3e7a240546f366c7e8
lizhihao97 <940166841@qq.com>
2018-04-20 21:36:57 +0800

    增加了排行榜api配置和请求方法

---
+++ src/model/ranking.js
@@ -0,0 +1,33 @@
+import * as SongModel from './song'
+
+export class Ranking {
+    constructor (id, title, img, songs) {
+        this.id = id;
+        this.title = title;
+        this.img = img;
+        this.songs = songs;
+    }
+}
+
+export function createRankingByList(data) {
+    const songList = [];
+    data.songList.forEach(item => {
+        songList.push(new SongModel.Song(0, '', item.songname, '', 0, '', item.singername))
+    })
+
+    return new Ranking (
+        data.id,
+        data.toTitle,
+        data.picUrl,
+        songList
+    )
+}
+
+export function createRankingDetail(data) {
+    return new Ranking (
+        data.toID,
+        data.ListName,
+        data.pic_album,
+        []
+    )
+}
