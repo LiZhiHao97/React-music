d3d4fb8f8d10bb48a186e8ba92846a59ab75278f
lizhihao97 <940166841@qq.com>
2018-04-21 23:34:54 +0800

    构建了排行榜界面

--- src/api/ranking.js
+++ src/api/ranking.js
@@ -1,31 +1,28 @@
-import jsonp from './jsonp'
+import jsonp from "./jsonp"
+import {URL, PARAM, OPTION} from "./config"
 
-import { URL, PARAM, OPTION } from './config'
-
-export function getRankingList () {
+export function getRankingList() {
     const data = Object.assign({}, PARAM, {
         g_tk: 5381,
         uin: 0,
-        platform: 'h5',
+        platform: "h5",
         needNewCode: 1,
         _: new Date().getTime()
-    })
-
-    return jsonp(URL.ranking, data, OPTION)
+    });
+    return jsonp(URL.rankingList, data, OPTION)
 }
 
 export function getRankingInfo(topId) {
     const data = Object.assign({}, PARAM, {
         g_tk: 5381,
         uin: 0,
-        platform: 'h5',
+        platform: "h5",
         needNewCode: 1,
         tpl: 3,
-        page: 'detail',
-        type: 'top',
+        page: "detail",
+        type: "top",
         topid: topId,
         _: new Date().getTime()
-    })
-
+    });
     return jsonp(URL.rankingInfo, data, OPTION)
 }
--- src/components/ranking/Ranking.js
+++ src/components/ranking/Ranking.js
@@ -1,12 +1,85 @@
 import React from 'react'
+import Scroll from '../../common/scroll/Scroll'
+import Loading from '../../common/loading/Loading'
+import { getRankingList } from '../../api/ranking'
+import { CODE_SUCCESS } from '../../api/config'
+import * as RankingModel from '../../model/ranking'
+
 import './ranking.scss'
 
 class Ranking extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            loading: true,
+            rankingList: [],
+            refreshScroll: false
+        }
+    }
+
+    componentDidMount() {
+        getRankingList().then((res) => {
+            console.log("获取排行榜：");
+            if (res) {
+                console.log(res);
+                if (res.code === CODE_SUCCESS) {
+                    let topList = [];
+                    res.data.topList.forEach(item => {
+                        if (/MV/i.test(item.topTitle)) {
+                            return;
+                        }
+                        topList.push(RankingModel.createRankingByList(item));
+                    });
+                    this.setState({
+                        loading: false,
+                        rankingList: topList
+                    }, () => {
+                        //刷新scroll
+                        this.setState({refreshScroll:true});
+                    });
+                }
+            }
+        });
+    }
+
     render () {
         return (
             <div className="music-ranking">
-                开发中
-            </div> 
+                <Scroll refresh={this.state.refreshScroll}>
+                    <div className="ranking-list">
+                        {
+                            this.state.rankingList.map(ranking => {
+                                return (
+                                    <div className="ranking-wrapper" key={ranking.id}>
+                                        <div className="left">
+                                            <img src={ranking.img} alt={ranking.title}/>
+                                        </div>
+                                        <div className="right">
+                                            <h1 className="ranking-title">
+                                                {ranking.title}
+                                            </h1>
+                                            {
+                                                ranking.songs.map((song, index) => {
+                                                    return (
+                                                        <div className="top-song" key={index}>
+                                                            <span className="index">{index + 1}</span>
+                                                            <span>{song.name}</span>
+                                                            &nbsp;-&nbsp;
+                                                            <span className="song">{song.singer}</span>
+                                                        </div>
+                                                    );
+                                                })
+                                            }
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
+
+                    </div>
+                </Scroll>
+                <Loading title="正在加载..." show={this.state.loading}/>
+            </div>
         )
     }
 }
